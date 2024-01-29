5c479dd7ac67a03e4d3e4200a03d5a6699c83a94
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-01-29 22:34:49 +0530

    Add files via upload

---
+++ Class/Car_record.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Car
+{
+    public :
+    int car_id;        
+    char car_colour[50];        
+    char car_model[50];        
+    int car_release_year;
+    char car_company_name[50];                
+};
+
+int main()
+{
+    int n;//number of cars
+    cout << "Enter the number of cars : ";
+    cin >> n;//user input
+    Car car[n];
+    int i;//loop counter
+
+    for(i=0; i<n; i++)
+    {
+        cout << endl;
+        cout << "Car " << i+1;
+        cout << endl;
+        cout << "Enter car ID : ";
+        cin >> car[i].car_id;
+        
+        cout << "Enter car company name : ";
+        cin >> car[i].car_company_name;
+        
+        cout << "Enter car colour : ";
+        cin >> car[i].car_colour;
+        
+        cout << "Enter car model : ";
+        cin >> car[i].car_model;
+        
+        cout << "Car release year : ";
+        cin >> car[i].car_release_year;
+    }
+    for(i=0; i<n; i++)
+    {
+        cout << endl;
+        cout << "Car " << i+1;
+        cout << endl;
+        cout << "Car ID : " << car[i].car_id << endl;
+        cout << "Car company name : " << car[i].car_company_name << endl;
+        cout << "Car colour : " << car[i].car_colour << endl;
+        cout << "Car model : " << car[i].car_model << endl;
+        cout << "Car release year : " << car[i].car_release_year << endl;
+        cout << endl;
+    }
+    return 0;
+}

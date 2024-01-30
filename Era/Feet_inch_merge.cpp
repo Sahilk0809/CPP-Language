a2f40ef359291b935737da3ed89f7b1bd6fc8897
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-01-30 15:18:47 +0530

    Add files via upload

---
+++ Era/Feet_inch_merge.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Merge
+{
+	public : 
+	int feet1,inch1,feet2,inch2;
+	
+	void ans()
+	{
+		cout << "Enter feet 1 : ";
+		cin >> feet1;
+		cout << "Enter feet 2 : ";
+		cin >> feet2;
+		cout << "Enter inch 1 : ";
+		cin >> inch1;
+		cout << "Enter inch 2 : ";
+		cin >> inch2;	
+	
+		feet1 = feet1 + (inch1/12);
+		inch1 = inch1%12;
+		feet2 = feet2 + (inch2/12);
+		inch2 = inch2%12;
+	
+		cout << endl << "Feet : " << feet1 + feet2;
+		cout << endl << "Inch : " << inch1 + inch2;
+	}
+};
+
+int main()
+{
+	Merge merge1;
+	
+	merge1.ans();
+	return 0;
+}

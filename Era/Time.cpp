bd73e277894f0b5e5b13f7c05b7f468c64183518
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-02-03 11:18:08 +0530

    Update Time.cpp

--- Era/Time.cpp
+++ Era/Time.cpp
@@ -9,21 +9,23 @@ class Time
 	
 	void process()
 	{
+//process to convert in hh:mm:ss
 		hr = second/3600;
 		min = (second%3600)/60;
-		
+		sec = second%60;
 	}
 	void print()
 	{
-		cout << hr << " " << min << " ";	
+		cout << hr << ":" << min << ":" << sec;	
 	}	
 };
 
 int main()
 {
+//object of class Time
 	Time time1;
 	cout << "Enter second : ";
-	cin >> time1.second;
+	cin >> time1.second;//user input of second
 	time1.process();
 	time1.print();
 	
